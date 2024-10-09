0034ca6e3d067cacb00cd226a6155cfee2d55bf2
TomAguer <tomaguer@orange.fr>
2024-10-09 17:14:53 +0200

    Player ignorer pour Raycast (Layer)

--- SAE 501 DREAM BOUND/Assets/Scrips/InteractionManager.cs
+++ SAE 501 DREAM BOUND/Assets/Scrips/InteractionManager.cs
@@ -9,48 +9,48 @@ namespace TomAg
         public float interactionRange = 3f;
         private Camera _camera;
 
+        // Ajoutez une variable LayerMask pour définir les couches à ignorer
+        public LayerMask ignoreLayers;
+
         private void Start()
         {
-          
             _camera = Camera.main;
         }
 
         private void OnEnable()
         {
-            
             FindObjectOfType<PlayerController>().onInteract += HandleInteraction;
         }
 
         private void OnDisable()
-{
-    var playerController = FindObjectOfType<PlayerController>();
-    if (playerController != null)
-    {
-        playerController.onInteract -= HandleInteraction;
-    }
-}
+        {
+            var playerController = FindObjectOfType<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.onInteract -= HandleInteraction;
+            }
+        }
 
         private void HandleInteraction()
-{
-    Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
-    RaycastHit hit;
-
-    if (Physics.Raycast(ray, out hit, interactionRange))
-    {
-        Debug.Log($"Raycast hit: {hit.collider.name}");
-
-       
-        if (hit.collider.CompareTag("Interactable"))
         {
-            Debug.Log("Interactable object detected");
-            hit.collider.GetComponent<InteractableObject>()?.Interact();
-        }
-    }
-    else
-    {
-        Debug.Log("No object hit by Raycast");
-    }
-}
+            Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
 
+            // Modifiez le Raycast pour utiliser le LayerMask
+            if (Physics.Raycast(ray, out hit, interactionRange, ~ignoreLayers)) // ~ pour inverser le LayerMask
+            {
+                Debug.Log($"Raycast hit: {hit.collider.name}");
+
+                if (hit.collider.CompareTag("Interactable"))
+                {
+                    Debug.Log("Interactable object detected");
+                    hit.collider.GetComponent<InteractableObject>()?.Interact();
+                }
+            }
+            else
+            {
+                Debug.Log("No object hit by Raycast");
+            }
+        }
     }
 }
