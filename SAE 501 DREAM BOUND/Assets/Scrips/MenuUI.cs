1891ed1e02b157243405c37f0b31552223ac0f2c
TomAguer <tomaguer@orange.fr>
2024-11-07 20:28:42 +0100

    Menu avec UI Toolkit

---
+++ SAE 501 DREAM BOUND/Assets/Scrips/MenuUI.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MenuUI : MonoBehaviour
+{
+    public NetworkManager multi;
+
+    private void OnEnable()
+    {
+        UIDocument uiDocument = GetComponent<UIDocument>();
+        VisualElement root = uiDocument.rootVisualElement;
+
+        Button createhost = root.Q<Button>("createhost");
+        Button joinsession = root.Q<Button>("joinsession");
+        Label codeLabel = root.Q<Label>("code");  
+        TextField inputField = root.Q<TextField>("inputfield");  
+        VisualElement menu = root.Q<VisualElement>("menu");  
+
+        createhost.clicked += () =>
+        {
+            multi.CreateMultiplayerRelay(codeLabel); 
+            createhost.style.display = DisplayStyle.None;
+                menu.style.display = DisplayStyle.None;
+            
+        };
+
+        joinsession.clicked += () =>
+        {
+            if (inputField != null)
+            {
+                multi.JoinSession(inputField.text); 
+            }
+            else
+            {
+                Debug.LogError("Input field for session code not found.");
+            }
+
+
+                menu.style.display = DisplayStyle.None;
+
+        };
+    }
+}
