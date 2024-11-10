a6c3945b599694a2243da02e2bf3ec3ac983830d
AxelDoussoux <axeldoussoux@gmail.com>
2024-11-10 23:54:17 +0100

    version a débugé

--- SAE 501 DREAM BOUND/Assets/Scrips/SpawnerManager.cs
+++ SAE 501 DREAM BOUND/Assets/Scrips/SpawnerManager.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class SpawnerManager : MonoBehaviour
+public class SpawnerManager : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject player1Prefab;
+    [SerializeField] private GameObject player2Prefab;
+    [SerializeField] private Transform spawn1Position;
+    [SerializeField] private Transform spawn2Position;
+
+    // ClientRpc pour informer les clients de leur joueur assigné
+    [ClientRpc]
+    private void AssignPlayerClientRpc(ulong clientId, NetworkObjectReference playerRef)
     {
-        
+        if (NetworkManager.Singleton.LocalClientId == clientId)
+        {
+            if (playerRef.TryGet(out NetworkObject playerObject))
+            {
+                Debug.Log($"Client {clientId} assigned to player {playerObject.gameObject.name}");
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        
+        if (!IsServer) return;
+
+        NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        if (!IsServer) return;
+
+        // Premier joueur connecté
+        if (clientId == NetworkManager.Singleton.ServerClientId)
+        {
+            SpawnPlayerForClient(clientId, player1Prefab, spawn1Position);
+        }
+        // Deuxième joueur connecté
+        else
+        {
+            SpawnPlayerForClient(clientId, player2Prefab, spawn2Position);
+        }
+    }
+
+    private void SpawnPlayerForClient(ulong clientId, GameObject playerPrefab, Transform spawnPoint)
+    {
+        GameObject playerInstance = Instantiate(
+            playerPrefab,
+            spawnPoint.position,
+            spawnPoint.rotation
+        );
+
+        NetworkObject netObj = playerInstance.GetComponent<NetworkObject>();
+        netObj.SpawnWithOwnership(clientId);
+
+        AssignPlayerClientRpc(clientId, netObj);
+
+        Debug.Log($"Spawned player for client {clientId}");
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+        }
     }
 }
