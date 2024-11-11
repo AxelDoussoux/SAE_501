2aed51ff217131da4aac2c3834d0a6fcbf4ac747
Maureen LAMBERT <maureen.lambert@free.fr>
2024-11-11 14:21:35 +0100

    Mécanisme tuto block

---
+++ SAE 501 DREAM BOUND/Assets/Scripts/TutoScripts/MovementSettings.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MovementSettings
+{
+    public Vector3 moveDirection = Vector3.up * 4f;
+    public float moveSpeed = 2f;
+}
