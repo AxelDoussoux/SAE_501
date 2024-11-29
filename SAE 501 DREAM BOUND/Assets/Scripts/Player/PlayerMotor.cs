0408ffca10c7a80219f43477ca1f186dd9500537
AxelDoussoux <axeldoussoux@gmail.com>
2024-11-29 16:31:43 +0100

    merge de main dans map (faut fixer le netcode et le relay mon gars tom)

--- SAE 501 DREAM BOUND/Assets/Scripts/Player/PlayerMotor.cs
+++ SAE 501 DREAM BOUND/Assets/Scripts/Player/PlayerMotor.cs
@@ -1,3 +1,7 @@
+Ôªøusing System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace TomAg
@@ -5,34 +9,79 @@ namespace TomAg
     public class PlayerMotor : MonoBehaviour
     {
         [Header("Movement")]
-        [SerializeField] private float walkForce = 100;
-        [SerializeField] private float strafeForce = 80;
+        [SerializeField]
+        private float walkForce = 510;
+        [SerializeField]
+        private float strafeForce = 450;
 
         [Header("Rotation")]
-        [SerializeField] private float rotationSensivity = 10;
-        [SerializeField] private Transform cameraTransform; // RÈfÈrence ‡ la camÈra
+        [SerializeField] 
+        private float rotationSensitivity = 10;
+        [SerializeField] 
+        private Transform cameraTransform; 
 
         [Header("Jump")]
-        [SerializeField] private float jumpForce = 10;
+        [SerializeField]
+        public float jumpForce = 100f;
+        [SerializeField]
+        private float movementAttenuationOnJump = 0.5f;
+        // [SerializeField]
+        // private float coyoteTime = 0.2f;
+
+        [Header("Ground")]
+        [SerializeField]
+        private LayerMask groundMask;
+        [SerializeField]
+        private float groundMaxDistance = 0.3f;
+        [SerializeField]
+        private float groundMaxDistanceAfterJump = 0.05f;
+        [SerializeField]
+        private float dragOnGround = 8f;
+        [SerializeField]
+        private float movementAttenuationInAir = 0.1f;
+
+        [Header("Spring")]
+        [SerializeField]
+        private float rideSpringStrength = 1f;
+        [SerializeField]
+        private float rideSpringDampener = 1f;
+        [SerializeField]
+        private float rideSpringMaxDistance = 0.2f;
+        [SerializeField]
+        private float rideSpringHeight = 1f;
+        [SerializeField]
+        private float rideSpringJumpTime = 0.3f;
 
         private Rigidbody _rb;
         private PlayerController _controller;
+
         private Vector3 _moveInput;
         private float _localOffsetAngleY;
         private float _rotationAngleY;
+        private bool _isGrounded;
+        private bool _isJumping;
+        private float _lastJumpTime;  
+        private float _lastGroundedTime;  
+
+        public bool isGrounded => _isGrounded;
+
+        public delegate void GroundChangeHandler(bool isGrounded);
+        public event GroundChangeHandler onGroundChanged; 
+
+        public delegate void JumpHandler();
+        public event JumpHandler onJump;  
 
         private void Awake()
         {
-            if (!TryGetComponent(out _rb))
-                Debug.LogError("Missing Rigidbody", this);
             if (!TryGetComponent(out _controller))
                 Debug.LogError("Missing PlayerController", this);
 
-            // VÈrifie que la camÈra est assignÈe
+            if (!TryGetComponent(out _rb))
+                Debug.LogError("Missing Rigidbody", this);
+
             if (cameraTransform == null)
                 cameraTransform = Camera.main.transform;
 
-            // Add event hooks
             _controller.onMove += OnMove;
             _controller.onAim += OnAim;
             _controller.onJumpStart += OnJumpStart;
@@ -43,7 +92,116 @@ namespace TomAg
 
         private void FixedUpdate()
         {
+            UpdateGrounded();
+            UpdateSpring();
+            UpdateDrag();
+            //UpdateRotation();
             UpdateMove();
+
+        }
+
+        private void UpdateGrounded()
+        {
+            // Skip calculation 50% of the time (1 frame every 2 frames)
+            if (Time.frameCount % 2 == 0)
+                return;
+
+            var origin = transform.position + transform.up;
+            var direction = -transform.up;
+            float distance = 1 + groundMaxDistance;
+
+            if (PlayerHasStartedJumping())
+                distance = 1 + groundMaxDistanceAfterJump;
+
+            bool wasGrounded = _isGrounded; // Sauvegarde l'√©tat pr√©c√©dent
+
+            if (Physics.Raycast(origin, direction, distance, groundMask, QueryTriggerInteraction.Ignore))
+            {
+                _isGrounded = true;
+
+                if (!wasGrounded)
+                {
+                    onGroundChanged?.Invoke(true);
+                    _isJumping = false; // Si au sol, alors il ne saute plus
+                    _rb.drag = 8f; // Assurez-vous de d√©finir le drag imm√©diatement lors de l'atterrissage
+                }
+
+                _lastGroundedTime = Time.timeSinceLevelLoad;
+                Debug.DrawLine(origin, origin + direction * distance, Color.green);
+            }
+            else
+            {
+                _isGrounded = false;
+
+                if (wasGrounded)
+                {
+                    onGroundChanged?.Invoke(false);
+                    _rb.drag = 1f; // D√©finir le drag √† 1 quand il quitte le sol
+                }
+            }
+        }
+
+        /* Ancien UpdateGrounded
+         * 
+         * private void UpdateGrounded()
+        {
+            // Skip calculation 50% of the time (1 frame every 2 frames)
+            if (Time.frameCount % 2 == 0)
+                return;
+
+            var origin = transform.position + transform.up;
+            var direction = -transform.up;
+            float distance = 1 + groundMaxDistance;
+
+            if (PlayerHasStartedJumping())
+                distance = 1 + groundMaxDistanceAfterJump;
+
+            if (Physics.Raycast(origin, direction, distance, groundMask, QueryTriggerInteraction.Ignore))
+            {
+                // Event
+                if (!_isGrounded)
+                    onGroundChanged?.Invoke(true);
+
+                // State
+                _isGrounded = true;
+
+                // Has landed after jump
+                if (!PlayerHasStartedJumping())
+                    _isJumping = false;
+
+                // Grounded time
+                _lastGroundedTime = Time.timeSinceLevelLoad;
+
+                // Debug
+                Debug.DrawLine(origin, origin + direction * distance, Color.green);
+            }
+        }*/
+
+        private bool PlayerHasStartedJumping() 
+        {
+            return Time.timeSinceLevelLoad < _lastJumpTime + rideSpringJumpTime;
+        }
+
+        private void UpdateDrag()
+        {
+
+            if (_isGrounded)
+            {
+                _rb.drag = dragOnGround;
+            }
+            else
+            {
+                _rb.drag = 0;
+                ApplyAirAttenuation();
+            }
+        }
+
+        private void ApplyAirAttenuation()
+        {
+            // Apply attenuation when in the air to simulate reduced control
+            Vector3 horizontalVelocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+            Vector3 attenuatedVelocity = horizontalVelocity * movementAttenuationInAir;
+            _rb.velocity = new Vector3(attenuatedVelocity.x, _rb.velocity.y, attenuatedVelocity.z);
         }
 
         private void OnMove(Vector2 axis)
@@ -53,16 +211,33 @@ namespace TomAg
 
         private void OnAim(Vector2 axis)
         {
-            _localOffsetAngleY = axis.x * rotationSensivity;
+            _localOffsetAngleY = axis.x * rotationSensitivity;
         }
 
         private void OnJumpStart()
         {
-            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            if (_isJumping) 
+                return;
+
+            if (!PlayerIsGrounded()) 
+                return;
+
+            _rb.velocity *= movementAttenuationOnJump;
+            _rb.AddRelativeForce(0, jumpForce, 0, ForceMode.Impulse);
+            _lastJumpTime = Time.timeSinceLevelLoad;
+            _isJumping = true;
+            _rb.drag = 1f;
+
+            onJump?.Invoke(); 
         }
 
-        private void OnJumpStop() { }
+        private void OnJumpStop()
+        {
+            _isJumping = false;
+        }
+
         private void OnCrouchStart() { }
+
         private void OnCrouchStop() { }
 
         private void UpdateMove()
@@ -70,7 +245,7 @@ namespace TomAg
             if (_moveInput == Vector3.zero)
                 return;
 
-            // Obtient la rotation de la camÈra (en ignorant la rotation X/inclinaison)
+            // Obtient la rotation de la camÔøΩra (en ignorant la rotation X/inclinaison)
             Vector3 cameraForward = cameraTransform.forward;
             Vector3 cameraRight = cameraTransform.right;
 
@@ -80,7 +255,7 @@ namespace TomAg
             cameraForward.Normalize();
             cameraRight.Normalize();
 
-            // Calcule la direction du mouvement basÈe sur la rotation de la camÈra
+            // Calcule la direction du mouvement basÔøΩe sur la rotation de la camÔøΩra
             Vector3 moveDirection = (cameraForward * _moveInput.y * walkForce) +
                                   (cameraRight * _moveInput.x * strafeForce);
 
@@ -91,8 +266,62 @@ namespace TomAg
             if (moveDirection != Vector3.zero)
             {
                 Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-                _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, targetRotation, Time.fixedDeltaTime * rotationSensivity));
+                _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, targetRotation, Time.fixedDeltaTime * rotationSensitivity));
             }
+        }
+
+        private bool PlayerIsGrounded()
+        {
+            var origin = transform.position + transform.up;
+            var direction = -transform.up;
+            float distance = 1 + groundMaxDistance;
+
+            if (PlayerHasStartedJumping())
+                distance = 1 + groundMaxDistanceAfterJump;
+
+            return Physics.Raycast(origin, direction, distance, groundMask, QueryTriggerInteraction.Ignore);
+        }
+
+
+        private void UpdateSpring()
+        {
+            if (_isJumping)
+                return;
+
+            var origin = transform.position + transform.up;
+            var direction = -transform.up;
+            float distance = 1 + rideSpringMaxDistance;
+
+            if (Physics.Raycast(origin, direction, out RaycastHit hit, distance, groundMask, QueryTriggerInteraction.Ignore))
+            {
+                // Calculate velocities on direction
+                float playerVelocityOnDirection = Vector3.Dot(direction, _rb.velocity);
+                float hitBodyVelocityOnDirection = hit.rigidbody != null ? Vector3.Dot(direction, hit.rigidbody.velocity) : 0;
+
+                // Resulting velocity
+                float relativeVelocity = playerVelocityOnDirection - hitBodyVelocityOnDirection;
+
+                // Distance inside hit object
+                float x = hit.distance - rideSpringHeight;
+
+                // Calculate the Force to apply
+                float springForce = (x * rideSpringStrength) - (relativeVelocity * rideSpringDampener);
+
+                // Apply force to Player
+                _rb.AddForce(direction * springForce);
+
+                // Apply force to hit body
+                if (hit.rigidbody != null)
+                    hit.rigidbody.AddForceAtPosition(-direction * springForce, hit.point);
+            }
+        }
+
+        private void UpdateRotation()
+        {
+            // Mettre √† jour l'angle de rotation en fonction de l'axe de vis√©e
+            _rotationAngleY += _localOffsetAngleY * Time.fixedDeltaTime;
+            Quaternion newRotation = Quaternion.Euler(0, _rotationAngleY, 0);
+            _rb.MoveRotation(newRotation);
         }
     }
 }
