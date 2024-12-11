2904565854a3215515c3602f078af21a8ca850b1
TomAguer <tomaguer@orange.fr>
2024-12-11 17:55:44 +0100

    Update ConversationStarter.cs
    correction erreur

--- SAE 501 DREAM BOUND/Assets/Scripts/ConversationStarter.cs
+++ SAE 501 DREAM BOUND/Assets/Scripts/ConversationStarter.cs
@@ -5,47 +5,15 @@ using DialogueEditor;
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
-    private Label dialogueText;
-    private VisualElement optionsContainer;
-
-    private void Start()
-    {
-        var uiDocument = FindObjectOfType<UIDocument>();
-        var root = uiDocument.rootVisualElement;
-
-        dialogueText = root.Q<Label>("DialogueText");
-        optionsContainer = root.Q<VisualElement>("OptionsContainer");
-    }
-
-    private void OnTriggerStay(Collider other)
+    
+   private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.F)) 
             {
-                StartDialogue();
+                ConversationManager.Instance.StartConversation(myConversation);
             }
         }
-    }
-
-    private void StartDialogue()
-    {
-        ConversationManager.Instance.StartConversation(myConversation);
-
-        // Example to update UI Toolkit dynamically
-        dialogueText.text = "This is a sample dialogue";
-        optionsContainer.Clear();
-
-        // Adding options dynamically
-        var option1 = new Button(() => OnOptionSelected(1)) { text = "Option 1" };
-        var option2 = new Button(() => OnOptionSelected(2)) { text = "Option 2" };
-        optionsContainer.Add(option1);
-        optionsContainer.Add(option2);
-    }
-
-    private void OnOptionSelected(int optionIndex)
-    {
-        Debug.Log($"Option {optionIndex} selected.");
-        // Implement logic for option handling here
     }
 }
