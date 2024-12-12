74cf64a8b2572c6bff1d41bbba3238d92d166f52
TomAguer <tomaguer@orange.fr>
2024-12-12 01:22:06 +0100

    Rangement + Utilisation Input System

--- SAE 501 DREAM BOUND/Assets/Scripts/DialogueEditor/ConversationStarter.cs
+++ SAE 501 DREAM BOUND/Assets/Scripts/DialogueEditor/ConversationStarter.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 using DialogueEditor;
 
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
-    
-   private void OnTriggerStay(Collider other)
+
+    private PlayerInput playerInput;
+    private InputAction interactAction;
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetKeyDown(KeyCode.F)) 
+            // Récupérer le PlayerInput si ce n'est pas déjà fait
+            if (playerInput == null)
+            {
+                playerInput = other.GetComponent<PlayerInput>();
+                if (playerInput != null)
+                {
+                    // Récupérer l'action Interact
+                    interactAction = playerInput.actions["Interact"];
+                }
+            }
+
+            // Vérifier si l'action Interact est déclenchée
+            if (interactAction != null && interactAction.triggered)
             {
                 ConversationManager.Instance.StartConversation(myConversation);
             }
