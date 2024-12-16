f976a24da8085fe89a39303fb1a72ca49aa33b43
TomAguer <tomaguer@orange.fr>
2024-12-16 17:30:22 +0100

    Ajout Stop NPC controller (bug à revoir)

---
+++ SAE 501 DREAM BOUND/Assets/Scripts/DialogueEditor/DialogueMorpheePatrouille1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueMorpheePatrouille1 : MonoBehaviour
+{
+    public void EnableCursor()
+    {
+        Cursor.lockState = CursorLockMode.None; // Libère le curseur
+        Cursor.visible = true; // Rendre le curseur visible
+    }
+
+
+}
