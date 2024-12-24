ec98672fb2ba7600d029122c89b8daa9e1370357
TomAguer <tomaguer@orange.fr>
2024-12-24 03:16:27 +0100

    Fix bug interaction chat + Ajout message lorsque l'on peut interagir

--- SAE 501 DREAM BOUND/Assets/Scripts/DialogueEditor/ConversationStarter.cs
+++ SAE 501 DREAM BOUND/Assets/Scripts/DialogueEditor/ConversationStarter.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using DialogueEditor;
 using TomAg;
+using TMPro;
 
 public class ConversationStarter : MonoBehaviour, IInteractable
 {
     [SerializeField] private NPCConversation myConversation;
+    [SerializeField] private TextMeshProUGUI interactionText; // Référence au texte UI
+    [SerializeField] private string message = "Appuie sur E pour interagir";
 
     private PlayerInput playerInput;
     private InputAction interactAction;
+    private bool isPlayerInRange = false;
+    private PlayerInfo currentPlayerInfo;
 
     // Implémentation de l'interface IInteractable
     public void Interact(PlayerInfo playerInfo)
@@ -18,6 +23,12 @@ public class ConversationStarter : MonoBehaviour, IInteractable
             // Démarrer la conversation avec le NPC
             ConversationManager.Instance.StartConversation(myConversation);
             Debug.Log($"Conversation démarrée avec le joueur : {playerInfo.name}");
+
+            // Cache le texte d'interaction
+            if (interactionText != null)
+            {
+                interactionText.gameObject.SetActive(false);
+            }
         }
         else
         {
@@ -25,29 +36,55 @@ public class ConversationStarter : MonoBehaviour, IInteractable
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        // Vérifie si le joueur possède un PlayerInfo (plus sûr qu'un tag)
         var playerInfo = other.GetComponent<PlayerInfo>();
         if (playerInfo != null)
         {
+            isPlayerInRange = true;
+            currentPlayerInfo = playerInfo; // Stocke la référence pour une utilisation ultérieure
+
+            // Affiche le texte d'interaction
+            if (interactionText != null)
+            {
+                interactionText.text = message;
+                interactionText.gameObject.SetActive(true);
+            }
+
             // Récupérer le PlayerInput si ce n'est pas déjà fait
             if (playerInput == null)
             {
                 playerInput = other.GetComponent<PlayerInput>();
                 if (playerInput != null)
                 {
-                    // Récupérer l'action Interact
                     interactAction = playerInput.actions["Interact"];
                 }
             }
+        }
+    }
 
-            // Si le joueur a pressé le bouton d'interaction
-            if (interactAction != null && interactAction.triggered)
+    private void OnTriggerExit(Collider other)
+    {
+        var playerInfo = other.GetComponent<PlayerInfo>();
+        if (playerInfo != null)
+        {
+            isPlayerInRange = false;
+            currentPlayerInfo = null;
+
+            // Cache le texte d'interaction
+            if (interactionText != null)
             {
-                // Appelle la méthode Interact via l'interface
-                Interact(playerInfo);
+                interactionText.gameObject.SetActive(false);
             }
+        }
+    }
+
+    private void Update()
+    {
+        // Vérifie si le joueur est dans la zone et a pressé l'action "Interact"
+        if (isPlayerInRange && interactAction != null && interactAction.triggered)
+        {
+            Interact(currentPlayerInfo);
         }
     }
 }
--- SAE 501 DREAM BOUND/Assets/Scripts/Player/PlayerInfo.cs
+++ SAE 501 DREAM BOUND/Assets/Scripts/Player/PlayerInfo.cs
@@ -1,5 +1,6 @@
 using Unity.Netcode;
 using UnityEngine;
+using DialogueEditor;
 
 namespace TomAg
 {
