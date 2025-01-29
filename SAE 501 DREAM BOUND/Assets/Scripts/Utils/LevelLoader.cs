95fcccd99b724a8d0c608f3d7c91d7c08a706103
AxelDoussoux <axeldoussoux@gmail.com>
2025-01-29 20:21:14 +0100

    Fix Script

--- SAE 501 DREAM BOUND/Assets/Scripts/Utils/LevelLoader.cs
+++ SAE 501 DREAM BOUND/Assets/Scripts/Utils/LevelLoader.cs
@@ -6,16 +6,19 @@ public class LevelLoader : NetworkBehaviour
     [SerializeField] private GameObject prefabToDisable;
     [SerializeField] private GameObject prefabToEnable;
 
+    // Subscribes to the level load event
     private void OnEnable()
     {
         LevelLoaderEvent.OnLevelLoad += HandleLevelLoad;
     }
 
+    // Unsubscribes from the level load event
     private void OnDisable()
     {
         LevelLoaderEvent.OnLevelLoad -= HandleLevelLoad;
     }
 
+    // Handles the level load event
     private void HandleLevelLoad()
     {
         if (IsServer)
@@ -24,12 +27,14 @@ public class LevelLoader : NetworkBehaviour
         }
     }
 
+    // Requests the server to switch prefabs
     [ServerRpc]
     private void SwitchPrefabsServerRpc()
     {
         SwitchPrefabsClientRpc();
     }
 
+    // Switches prefabs on all clients
     [ClientRpc]
     private void SwitchPrefabsClientRpc()
     {
@@ -44,5 +49,3 @@ public class LevelLoader : NetworkBehaviour
         }
     }
 }
-
-
